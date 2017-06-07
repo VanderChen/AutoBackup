210042d40ff79e2e4c50ec1fccb82eafe081bb3d
Michael <siyuan.arc@gmail.com>
2017-06-07 17:29:07 +0800

    Add paramiko module Test

--- grammerTest.py
+++ grammerTest.py
@@ -10,12 +10,12 @@ def sftp_upload_file(host, user, pwd):
         transport = paramiko.transport((host, 22))
         transport.connect(username=user, password=pwd)
         sftp = paramiko.SFTPClient.from_transport(transport)
-        for root, dirs, files in os.walk("pyinstaller", topdown=False):
+        for root, dirs, files in os.walk("testParamiko", topdown=False):
         	for name in files:
         		sftp.put(os.path.join(root, name),('~/bak/'+os.path.join(root, name)))
         pass
     except Exception as e:
-        print e
+        print (e)
 
 if __name__ == '__main__':
 	host = "114.215.142.52"
